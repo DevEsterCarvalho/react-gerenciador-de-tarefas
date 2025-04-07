e070a3ce52acaecdfd407e8d96d406506ddd6b31
DevEsterCarvalho <estercarvalho205@gmail.com>
2025-04-07 18:51:02 -0300

    adicionando fuction de remover e concluir tasks

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,32 @@ function App() {
 		},
 	]);
 
+	function onTasksClick(taskId) {
+		const newTasks = tasks.map((task) => {
+			if (task.id === taskId) {
+				return { ...task, isComplited: !task.isComplited };
+			}
+			return task;
+		});
+		setTasks(newTasks);
+	}
+
+	function removeTasks(taskId) {
+		const newTasks = tasks.filter((task) => task.id !== taskId);
+		setTasks(newTasks);
+	}
+
 	return (
 		<div className="w-screen h-screen bg-purple-400 flex justify-center p-6">
 			<div className="w-[500px]">
 				<h1 className="text-3xl text-slate-100 font-bold text-center">
 					Gerenciador de Tarefas
 				</h1>
-				<Tasks tasks={tasks} />
+				<Tasks
+					tasks={tasks}
+					onTasksClick={onTasksClick}
+					removeTasks={removeTasks}
+				/>
 			</div>
 		</div>
 	);
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,15 +1,29 @@
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
-function Tasks(props) {
+function Tasks({ tasks, onTasksClick, removeTasks }) {
 	return (
 		<ul className="space-y-4 p-6 bg-purple-500 rounded-md shadow">
-			{props.tasks.map((task) => (
-				<li key={Tasks.id} className="flex gap-2">
-					<button className=" w-full text-left bg-purple-300 text-white p-2 rounded-md">
+			{tasks.map((task) => (
+				<li key={task.id} className="flex gap-2">
+					<button
+						onClick={() => onTasksClick(task.id)}
+						className={` w-full text-left bg-purple-300 text-white p-2 rounded-md ${
+							task.isComplited && "line-through"
+						}`}
+					>
 						{task.title}
 					</button>
 					<button className="bg-purple-300 text-white p-2 rounded-md">
 						<ChevronRightIcon />
+					</button>
+
+					<button
+						onClick={() => removeTasks(task.id)}
+						className={` bg-purple-300 text-white p-2 rounded-md ${
+							task.remove && "line-through"
+						}`}
+					>
+						<TrashIcon />
 					</button>
 				</li>
 			))}
